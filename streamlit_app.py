02f10381e1cf78fd503df007b04372a573fd0a8d
abyanwibowo19400 <bowo.wbw1@gmail.com>
2025-05-15 02:06:10 +0700

    columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,19 @@
 import streamlit as st
 
-st.title("Belajar Streamlit 30 Hari by Grok AI")
-st.write("Ini adalah perjalan saya belajar streamlit dibantu oleh Grok AI")
-st.write("Saya mempunyai Hobi untuk belajar hal yang baru, walaupun terkadang saya cepat bosan")
-st.write("saya mengenal streamlit melalui teman asprak alpro saya, yaitu bima")
-st.write("Saya merasa hal ini adalah suatu hal yang baru karena tidak pernah diajarkan pada jurusan saya")
+col1, col2, col3 = st.columns(3)
+with col1:
+
+    st.title("Belajar Streamlit 30 Hari by Grok AI")
+    st.write("Ini adalah perjalan saya belajar streamlit dibantu oleh Grok AI")
+    st.write("Saya mempunyai Hobi untuk belajar hal yang baru, walaupun terkadang saya cepat bosan")
+    st.write("saya mengenal streamlit melalui teman asprak alpro saya, yaitu bima")
+    st.write("Saya merasa hal ini adalah suatu hal yang baru karena tidak pernah diajarkan pada jurusan saya")
+
+st.columns
+with col2:
+    tombol = st.button("Tekan") 
+    reset = st.button("resset")
+    if tombol:
+        st.write("Tombol Ditekan")
+    else:
+        st.write("Tombol Tidak ditekan")
