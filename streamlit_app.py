6e33d1bef55d29b1528e1963eb933ed8c2e02840
abyanwibowo19400 <bowo.wbw1@gmail.com>
2025-05-14 21:58:07 +0700

    hobi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
-st.header('st.button')
-
-if st.button('Say hello'):
-     st.write('Why hello there')
-else:
-     st.write('Goodbye')
+st.title("Belajar Streamlit 30 Hari by Grok AI")
+st.write("Ini adalah perjalan saya belajar streamlit dibantu oleh Grok AI")
+st.write("Saya mempunyai Hobi untuk belajar hal yang baru, walaupun terkadang saya cepat bosan")
+st.write("saya mengenal streamlit melalui teman asprak alpro saya, yaitu bima")
+st.write("Saya merasa hal ini adalah suatu hal yang baru karena tidak pernah diajarkan pada jurusan saya")
